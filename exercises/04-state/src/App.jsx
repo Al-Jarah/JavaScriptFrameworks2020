76390ec5bb49beb0f063a3310123f3c63cf71577
Elijah <admin@elijahbrown.info>
2020-03-03 14:09:42 -0500

    imported lessText and characterCount
    Signed-off-by: Elijah <admin@elijahbrown.info>

--- exercises/04-state/src/App.jsx
+++ exercises/04-state/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import LessText from "./LessText";
-// Import CharacterCount here
+import CharacterCount from "./CharacterCount"
 
 function App() {
   return (
@@ -9,11 +9,15 @@ function App() {
       <div className="container pt-4 pb-4">
         <h2 className="h3">Challenge 1 - Less Text</h2>
         {/* Add LessText with props here */}
+        <LessText text={"hello"} maxLength={2} 
+
+        />
       </div>
       <div className="bg-secondary text-white pt-4 pb-4 mb-4">
         <div className="container">
           <h1 className="h3">Challenge 2 - Character Count</h1>
           {/* Add CharacterCount here */}
+          <CharacterCount/>
         </div>
       </div>
     </>
