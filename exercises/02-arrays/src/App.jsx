d7acd74a48284e57eb71c85cbbcd1176c4173e57
Elijah <admin@elijahbrown.info>
2020-03-25 18:15:36 -0400

    code cleanup
    Signed-off-by: Elijah <admin@elijahbrown.info>

--- exercises/02-arrays/src/App.jsx
+++ exercises/02-arrays/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import images from "./images/images"
 import "./App.css";
+import images from "./images/images";
 
 function App() {
-  console.log(images)
-  return <div className="App"><h1>{images.map((image) => {
-    console.log(image)
- return <img src = {image}/>
+  return <div className="App"><h1>{images.map((image,index) => {
+    return <img src={image} key={`image-${index}`}/>
   })}</h1></div>;
   
 }
