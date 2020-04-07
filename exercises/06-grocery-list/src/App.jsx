789326ffe18f586f8826ea500500921c420dd175
Elijah <admin@elijahbrown.info>
2020-04-07 13:02:44 -0400

    refractored code
    Signed-off-by: Elijah <admin@elijahbrown.info>

--- exercises/06-grocery-list/src/App.jsx
+++ exercises/06-grocery-list/src/App.jsx
@@ -3,34 +3,34 @@ import "./App.css";
 
 const GroceryList = () => {
 
-  const [groceryItem, setGroceryItem] = useState();
-  const [cost,setCost] = useState();
-  const [groceryList,setGroceryList] = useState([]);
+  const [groceryItem, setGroceryItem] = useState("");
+  const [cost, setCost] = useState("");
+  const [groceryList, setGroceryList] = useState([]);
+  const [hasError, setHasError] = useState(false);
 
 
-  
-  const updateItem = (userInput, index) => {
-    const newgroceryItem = [...groceryItem];
-    newgroceryItem[index] = userInput;
-    setGroceryItem(newgroceryItem);
-  };
-  const deleteItem = index => {
-    setGroceryItem(
-      groceryItem.filter((item, currentIndex) => currentIndex !== index)
+  const updateList = () => {
+    setGroceryList([...groceryList, { groceryItem, cost: parseFloat(cost) }])
+  }
+
+
+  const deleteItem = deleteIndex => {
+    setGroceryList(
+      [...groceryList].filter((item, currentIndex) => currentIndex !== deleteIndex)
     );
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const obj = {
-      item:groceryItem,
-      cost:cost
-    }
- const temp = [...groceryList];
-    temp.push(obj);
-    console.log(temp)
-    setGroceryList(temp)
+  const clearList = () => setGroceryList([]);
 
+  const calcTotal = () => {
+    return groceryList.reduce((total, item) => {
+      return total + item.cost;
+    }, 0)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+      (groceryItem && (cost || cost === 0)) ? updateList() && setHasError(false) : setHasError(true)
   }
   return (
     <div className="container">
@@ -42,8 +42,9 @@ const GroceryList = () => {
             name="item"
             placeholder="Name of grocery item..."
             aria-label="Name of grocery item..."
-            onChange = {(e)=> setGroceryItem(e.target.value)}
-            
+            value={groceryItem}
+            onChange={(e) => setGroceryItem(e.target.value)}
+
           />
           <input
             className="form-control"
@@ -52,50 +53,57 @@ const GroceryList = () => {
             step=".01"
             placeholder="Cost of grocery Item..."
             aria-label="Cost of grocery Item..."
-            onChange = {(e)=> setCost(e.target.value)}
+            value={cost}
+            onChange={(e) => setCost(e.target.value)}
           />
           <div>
-            <button type="submit" className="btn btn-success" onClick={newItem}>
+            <button type="submit" className="btn btn-success">
               Add
             </button>
           </div>
         </form>
+        {hasError && (
+          <p className="help-block text-danger"> Both fields are required.</p>
+        )}
       </div>
       <div className="card card-body border-white">
         <h1 className="h4">Grocery List</h1>
         <table className="table table-compact">
           <thead>
-          {/* {groceryItem.map((item, index) =>{return()})} */}
+            {/* {groceryItem.map((item, index) =>{return()})} */}
             <tr>
               <th>Item</th>
               <th>Cost</th>
               <th></th>
             </tr>
           </thead>
-          <tbody
-          {groceryList.map((userInput, inedx) => {
-          return (<td> {groceryItem}</td>)
-          })};
-            {/**
-             * Complete me. (You can use something else instead of a table if you like)
-             * @example
-             * <tr>
-             *   <td>Toilet Paper</td>
-             *   <td>$1.99</td>
-             *   <td>
-             *     <button aria-label="Delete" title="Delete" ... >
-             *       &times;
-             *     </button>
-             *   </td>
-             * </tr>
-             */}
-          />
+          <tbody>
+            {groceryList.map((item, index) => {
+              return (
+                <tr>
+                  <td>{item.groceryItem}</td>
+                  <td>${item.cost.toFixed(2)}</td>
+                  <td><button aria-label="Delete"
+                    title="Delete"
+                    className="btn"
+                    onClick={() => deleteItem(index)}>
+                    &times;
+                  </button>
+                  </td>
+                </tr>)
+            })}
+
+          </tbody>
         </table>
         <p className="lead">
-          <strong>Total Cost: {/* Complete me */}</strong>
+          <strong>Total Cost: {calcTotal().toFixed(2)}</strong>
         </p>
         <div className="text-right">
-          <button type="button" className="btn btn-outline-success">
+          <button
+            type="button"
+            className="btn btn-outline-success"
+            onClick={clearList}
+          >
             Clear
           </button>
         </div>
