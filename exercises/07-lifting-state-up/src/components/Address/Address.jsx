9d14a21319047789adeb20f855763b736ba2919e
Elijah <admin@elijahbrown.info>
2020-04-07 13:04:23 -0400

    imported address form and address resultes
    Signed-off-by: Elijah <admin@elijahbrown.info>

--- exercises/07-lifting-state-up/src/components/Address/Address.jsx
+++ exercises/07-lifting-state-up/src/components/Address/Address.jsx
@@ -1,19 +1,30 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import AddressForm from "../AddressForm/AddressForm"
+import AddressResults from "../AddressResults/AddressResults"
 
-import AddressForm from "../AddressForm/AddressForm";
-import AddressResults from "../AddressResults/AddressResults";
 
 function App() {
-  const [displayResults, setDisplayResults] = useState(false);
+  const [displayResults, setDisplayResults] = useState(false)
   /**
    * You will need to call on useState here for form fields
    * e.g. first name, last name, etc.
    */
-
+  const [formValues, setFormValues] = useState({})
   /**
    * You will need to pass props to <AddressResults /> and <AddressForm />
    */
-  return <>{displayResults ? <AddressResults /> : <AddressForm />}</>;
+  return (<>
+    {displayResults ? (<AddressResults formValues={formValues} />
+    ) : (
+        <AddressForm
+          formValues={formValues}
+          setFormValues={setFormValues}
+          setDisplayResults={setDisplayResults}
+
+        />
+      )}
+  </>
+  )
 }
 
 export default App;
